cf395d6a306d70726cc13d0bbb5779e7f0608a39
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-04-07 02:18:10 +0530

    game logic completed

--- client/src/Components/GameScreen/GameScreen.js
+++ client/src/Components/GameScreen/GameScreen.js
@@ -1,30 +1,85 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './GameScreen.css'
 const GameScreen = () => {
   const [isItX, setIsItX] = useState(true)
   const reset = () => {
     //reset functionality to be added
+    setDataSet(['', '', '', '', '', '', '', '', ''])
+    setIsItX(true)
+    setGameOver(false)
+    setWinner('')
+    setIsTie(false)
     console.log('reset')
   }
   const [dataSet, setDataSet] = useState(['', '', '', '', '', '', '', '', ''])
-
-  const gameFunc = (id) => {
-    if (isItX) {
-      let newArr = [...dataSet]
-      if (newArr[id] == '') {
-        newArr[id] = 'X'
-        setIsItX(!isItX)
+  const [gameOver, setGameOver] = useState(false)
+  const [winner, setWinner] = useState('')
+  const [isTie, setIsTie] = useState(false)
+  const checkIsTie = () => {
+    if (gameOver) {
+      return
+    }
+    for (let i = 0; i < 9; i++) {
+      if (dataSet[i] === '') {
+        return
       }
-      setDataSet(newArr)
-    } else {
-      let newArr = [...dataSet]
-      if (newArr[id] == '') {
-        newArr[id] = 'O'
-        setIsItX(!isItX)
+    }
+    setIsTie(true)
+  }
+  const findWinner = () => {
+    const CONDITIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [2, 5, 8],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < CONDITIONS.length; i++) {
+      let firstPos = CONDITIONS[i][0]
+      let secondPos = CONDITIONS[i][1]
+      let thirdPos = CONDITIONS[i][2]
+      if (
+        dataSet[firstPos] === dataSet[secondPos] &&
+        dataSet[secondPos] === dataSet[thirdPos] &&
+        dataSet[firstPos] != ''
+      ) {
+        setGameOver(true)
+        setWinner(dataSet[firstPos])
       }
-      setDataSet(newArr)
     }
   }
+
+  const gameFunc = (id) => {
+    let newArr = [...dataSet]
+    if (gameOver) {
+      return
+    }
+    if (isItX) {
+      if (newArr[id] == '') {
+        newArr[id] = 'X'
+        // setIsItX(!isItX)
+      }
+      // setDataSet(newArr)
+    } else {
+      // let newArr = [...dataSet]
+      if (newArr[id] == '') {
+        newArr[id] = 'O'
+      }
+    }
+    setIsItX(!isItX)
+    setDataSet(newArr)
+
+    findWinner()
+    checkIsTie()
+  }
+  useEffect(() => {
+    findWinner()
+    checkIsTie()
+    // console.log('winner:' + winner + ' draw:' + isTie)
+  }, [dataSet, gameOver, isTie])
   return (
     <div className='container'>
       <h1>MultiPlayer (hopefully) Tic-Tac-Toe</h1>
@@ -58,7 +113,9 @@ const GameScreen = () => {
         </div>
       </div>
       <h3 className='winner'></h3>
-      <button onClick={() => reset()}>Reset</button>
+      {gameOver && <h2>Winner is {winner}</h2>}
+      {!gameOver && isTie && <h2>Ugh! The game ended in a draw</h2>}
+      {(gameOver || isTie) && <button onClick={() => reset()}>Reset</button>}
     </div>
   )
 }
